c7d56cb815a3b3c5e56d43127b31006cefff6c86
devclyde <israel.devclyde@protonmail.com>
2022-06-25 20:11:49 -0300

    feat: load core signals

--- profiles/gigachad/init.lua
+++ profiles/gigachad/init.lua
@@ -1,1 +1,6 @@
-LOG('Hello, world!')
+require('gigachad.signals.naughty.display')
+require('gigachad.signals.naughty.display-error')
+
+require('gigachad.signals.tag.default-layouts')
+
+require('gigachad.signals.screen.wallpaper')
