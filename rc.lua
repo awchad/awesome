d9cd63954538c6b0a3628ec830d11683465d0190
devclyde <israel.devclyde@protonmail.com>
2022-06-26 11:31:36 -0300

    feat: create `rc.lua`

---
+++ rc.lua
@@ -0,0 +1,12 @@
+-- !!! rc.lua - AwesomeWM Startup Definition ............... -*- Lua -*- !!!
+--
+-- Part of AwChad project, under the GNU General Public License v3.
+-- See https://www.gnu.org/licenses/gpl-3.0.en.html for license information.
+-- SPDX-License-Identifier: GPL-3.0-only
+--
+-- !!! ................................................................. !!!
+---
+--- The code responsible for starting AwesomeWM. Contains global variables
+--- useful for developing this configuration.
+---
+-- !!! ................................................................. !!!
